3f5a2f7fd107abe3755a7ab7bd55aafa116e59a8
muradusa <muradusa@gmail.com>
2022-01-17 11:32:56 -0800

    finished with DTube contract in solidity

--- src/contracts/DTube.sol
+++ src/contracts/DTube.sol
@@ -1,41 +1,40 @@
 pragma solidity ^0.5.0;
 
 contract DTube {
-  uint public videoCount = 0;
-  string public name = "DTube";
-  mapping(uint => Video) public videos;
+    uint256 public videoCount = 0;
+    string public name = DTube;
+    mapping(uint256 => Video) public videos;
 
-  struct Video {
-    uint id;
-    string hash;
-    string title;
-    address author;
-  }
+    // Create data types
+    struct Video {
+        uint256 id; // vidoe count
+        string hash; //IPFS video hash
+        string title; // Title of the video
+        address author; //User's wallet address
+    }
 
-  event VideoUploaded(
-    uint id,
-    string hash,
-    string title,
-    address author
-  );
+    // Create an event
+    event VideoUploaded(uint256 id, string hash, string title, address author);
 
-  constructor() public {
-  }
+    constructor() public {}
 
-  function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
-    require(bytes(_videoHash).length > 0);
-    // Make sure video title exists
-    require(bytes(_title).length > 0);
-    // Make sure uploader address exists
-    require(msg.sender!=address(0));
+    function uploadVideo(string memory _videoHash, string memory _title)
+        public
+    {
+        // make sure the video exists
+        require(bytes(_videoHash).length > 0);
+        // make sure video title exists
+        require(bytes(_title).length > 0);
+        // make sure uploader address exists
+        require(msg.sender != address(0));
 
-    // Increment video id
-    videoCount ++;
+        // Increment video id
+        videoCount++;
 
-    // Add video to the contract
-    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
-    // Trigger an event
-    emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
-  }
+        // Add video to the contract
+        videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
+
+        // Trigger an event
+        emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
+    }
 }
